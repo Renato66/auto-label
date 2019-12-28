9a6b43e6390dd4172f5e1fbe4a11190959378e54
Renato Vicente <revi66@hotmail.com>
2019-12-28 01:05:05 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function run() {
 
     // Get github context data
     const context = github.context;
-    console.log(`We can even get context data, like the repo: ${context.repo.repo}`)
+    console.log(`We can even get context data, like the repo2: ${context}`)
   } catch (error) {
     core.setFailed(error.message);
   }
