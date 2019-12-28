4bff5e78f014054a93f3c6f33e69c85f9a2fd1d1
Renato66 <revi66@hotmail.com>
2019-12-28 05:05:56 -0300

    isHidden prop

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require('@actions/core')
+const github = require('@actions/github')
 
 async function run() {
   try {
-    const token = core.getInput('repo-token', {required: true});
-    const allowCreate = core.getInput('allow-create');
-    const client = new github.GitHub(token);
-    const context = github.context;
+    const token = core.getInput('repo-token', {required: true})
+    const allowCreate = core.getInput('allow-create')
+    const isHidden = core.getInput('is-hidden')
+    const client = new github.GitHub(token)
+    const context = github.context
 
-    let labels: string[] = getLabels(context.payload.issue.body)
+    let labels: string[] = getLabels(context.payload.issue.body, isHidden)
     console.log(`Labels found: ${labels.length}`)
     if (!allowCreate) {
       console.log('Removing uncreated labels...')
@@ -19,7 +20,7 @@ async function run() {
     console.log('Done')
     console.log(`${labels.length} labels added`)
   } catch (error) {
-    core.setFailed(error.message);
+    core.setFailed(error.message)
   }
 }
 
@@ -30,7 +31,7 @@ async function removeUncreatedLabels(
   const {data: list} = await client.issues.listLabelsForRepo({
     owner: github.context.repo.owner,
     repo: github.context.repo.repo
-  });
+  })
   console.log(list)
   const labelNames: object = {}
   list.forEach((elem: any) => {
@@ -49,13 +50,14 @@ async function addLabels(
     repo: github.context.repo.repo,
     issue_number: issueNumber,
     labels: labels
-  });
+  })
 }
 
-function getLabels (body: string) {
+function getLabels (body: string, isHidden: boolean) {
   let comentary: boolean = false
   let labels: string[] = []
-  body.split('\n').forEach((line: string) => {
+  if (isHidden) {
+    body.split('\n').forEach((line: string) => {
       if (line.includes('<!--')) {
         comentary = true
       } if (line.includes('-->')) {
@@ -66,8 +68,16 @@ function getLabels (body: string) {
             labels.push(line.split(/\[\s*x\s*\]/i)[1].trim())
         } 
       }
-  })
+    })
+  } else {
+    body.split('\n').forEach((line: string) => {
+      if ((/\[\s*x\s*\]/i).test(line)) {
+        labels.push(line.split(/\[\s*x\s*\]/i)[1].trim())
+      } 
+    })
+  }
+  
   return labels
 }
 
-run();
+run()
