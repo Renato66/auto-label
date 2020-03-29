8a0869822b99bdbda507570709f7f13f2bc21c53
Renato66 <revi66@hotmail.com>
2020-03-29 18:14:22 +0000

    Deploying to gh-pages from front - bcf5a066fb7eca4ce0f84fa6b86cf33a992deeae 🚀

---
+++ precache-manifest.1081d8a2898a17bad7f989d8a099b007.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "122a8b40a38002778e16",
+    "url": "/js/app.b1856797.js"
+  },
+  {
+    "revision": "d8aa16168fb440e60745",
+    "url": "/css/chunk-vendors.b6eb4e8d.css"
+  },
+  {
+    "revision": "d8aa16168fb440e60745",
+    "url": "/js/chunk-vendors.3929b4de.js"
+  },
+  {
+    "revision": "1e2fd8cf843914b0fce98a3a5458e2ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "982e11a6b318f40d19f9793eb1849d58",
+    "url": "/404.html"
+  },
+  {
+    "revision": "5a04fb676aaf13ba0886dbef5600aaac",
+    "url": "/img/lottie/404.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
