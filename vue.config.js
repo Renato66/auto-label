df574eaac5db1f01f4d9b3c9bd8ab0f9aa112ec8
Renato Vicente <revi66@hotmail.com>
2020-03-29 16:13:06 -0300

    publicPath url

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
-const path = require('path'); const
-  PrerenderSPAPlugin = require('prerender-spa-plugin')
-const Renderer = PrerenderSPAPlugin.PuppeteerRenderer
-
 module.exports = {
   'transpileDependencies': [
     'vuetify'
   ],
+
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/auto-label/'
+    : '/',
 
   pluginOptions: {
     i18n: {
@@ -13,31 +13,6 @@ module.exports = {
       fallbackLocale: 'en',
       localeDir: 'locales',
       enableInSFC: false
-    }
-  },
-
-  configureWebpack: config => {
-    if (process.env.NODE_ENV !== 'production') return
-    return {
-      plugins: [
-        new PrerenderSPAPlugin({
-          staticDir: path.join(__dirname, 'dist'),
-          routes: ['/'],
-          minify: {
-            collapseBooleanAttributes: true,
-            collapseWhitespace: true,
-            decodeEntities: true,
-            keepClosingSlash: true,
-            sortAttributes: true
-          },
-          server: {
-            port: 8001
-          },
-          renderer: new Renderer({
-            renderAfterDocumentEvent: 'render-event'
-          })
-        })
-      ]
     }
   }
 }
