ffb9a7811dab838af08dbc645905dedcc316e314
Renato Vicente <revi66@hotmail.com>
2024-05-03 14:43:19 +0200

    test: fix test on text runner

--- src/scraper/text.spec.ts
+++ src/scraper/text.spec.ts
@@ -5,36 +5,46 @@ describe('getIssueLabels function', () => {
   test('should return an array of labels extracted from body', () => {
     const body = 'Body with labels <!-- AUTO-LABEL:START --> Label1 Label2 <!-- AUTO-LABEL:END -->';
     const labels = ['Label1', 'Label2'];
-    const ignoreComments = true;
-    const defaultLabels = ['DefaultLabel1', 'DefaultLabel2'];
-    const labelsSynonyms = { 'Label1': ['Synonym1'], 'Label2': ['Synonym2'] };
 
-    const result = getIssueLabels(body, labels, ignoreComments, defaultLabels, labelsSynonyms);
+    const result = getIssueLabels(body, labels, false, [], {});
 
-    expect(result).toEqual(['Synonym1', 'Synonym2', 'DefaultLabel1', 'DefaultLabel2']);
+    expect(result).toEqual(['Label1', 'Label2']);
   });
 
   test('should handle no labels in body', () => {
     const body = 'No labels in this body';
     const labels = ['Label1', 'Label2'];
-    const ignoreComments = true;
-    const defaultLabels = ['DefaultLabel1', 'DefaultLabel2'];
-    const labelsSynonyms = { 'Label1': ['Synonym1'], 'Label2': ['Synonym2'] };
+    const result = getIssueLabels(body, labels, false, [], {});
 
-    const result = getIssueLabels(body, labels, ignoreComments, defaultLabels, labelsSynonyms);
+    expect(result).toEqual([]);
+  });
+
+  test('should add default labels', () => {
+    const body = 'No labels in this body';
+    const labels = ['Label1', 'Label2'];
+    const defaultLabels = ['DefaultLabel1', 'DefaultLabel2'];
+
+    const result = getIssueLabels(body, labels, false, defaultLabels, {});
 
     expect(result).toEqual(['DefaultLabel1', 'DefaultLabel2']);
   });
 
-  test('should return an array of labels extracted from body without ignoring comments', () => {
-    const body = 'Body with labels <!-- AUTO-LABEL:START --> Label1 Label2 <!-- AUTO-LABEL:END -->';
+  test('should not add labels inside the comments section', () => {
+    const body = 'Body with labels: Label1  <!-- the label Label2 is mentioned in the body, but its commented and shouldn`t be tracked -->';
     const labels = ['Label1', 'Label2'];
-    const ignoreComments = false;
-    const defaultLabels = ['DefaultLabel1', 'DefaultLabel2'];
+
+    const result = getIssueLabels(body, labels, true, [], {});
+
+    expect(result).toEqual(['Label1']);
+  });
+
+  test('should check if there is any synonym for the labels available', () => {
+    const body = 'Body with labels: Synonym1  <!-- the label Synonym2 is mentioned in the body, but its commented and shouldn`t be tracked -->';
+    const labels = ['Label1', 'Label2'];
     const labelsSynonyms = { 'Label1': ['Synonym1'], 'Label2': ['Synonym2'] };
 
-    const result = getIssueLabels(body, labels, ignoreComments, defaultLabels, labelsSynonyms);
+    const result = getIssueLabels(body, labels, true, [], labelsSynonyms);
 
-    expect(result).toEqual(['Synonym1', 'Synonym2', 'DefaultLabel1', 'DefaultLabel2']);
+    expect(result).toEqual(['Label1']);
   });
 });
